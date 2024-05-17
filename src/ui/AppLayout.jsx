9e012170da161e5152ecc563237f7e8f02d5a67b
damianx989 <damianx989@gmail.com>
2024-05-17 21:30:47 +0000

    added custom font family

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,4 @@
-import { Outlet } from "react-router-dom";
-import { useNavigation } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import Loader from "./Loader";
 import CartOverview from "../features/cart/CartOverview";
@@ -8,12 +7,17 @@ function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
+
+
+      <div className="overflow-scroll">
+      <main className="mx-auto max-w-3xl">
         <Outlet />
       </main>
+      </div>
+
       <CartOverview />
     </div>
   );
