2a9b0f7edfcd54698b6df5b96a7b600a0a499561
Damian Roiz <damianx989@gmail.com>
2024-05-17 21:21:25 -0400

    Update UI styles and layout in CartOverview, CreateUser, Username, AppLayout, Header, and Home components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,22 +1,26 @@
-import { Outlet } from "react-router-dom";
-import { useNavigation } from "react-router-dom";
-import Header from "./Header";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
+import { Outlet } from 'react-router-dom';
+import { useNavigation } from 'react-router-dom';
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
 }
+
+
 
 export default AppLayout;
